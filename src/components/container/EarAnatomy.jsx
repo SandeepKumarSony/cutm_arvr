0e685e1d539ba5b1f6ac0a33549946fc6c61c7e0
SandeepKumarSony <raj.aryan.sony@gmail.com>
2024-05-06 20:37:36 +0530

    commit

--- src/components/container/EarAnatomy.jsx
+++ src/components/container/EarAnatomy.jsx
@@ -5,7 +5,8 @@ const EarAnatomy = () => {
         <div className='flex justify-center items-center h-screen'> <iframe
             src="https://humanear-eta.vercel.app/"
             title="Ear"
-            style={{ width: "500px", height: "400px" }}
+            className='h-screen w-full'
+            //style={{ width: "500px", height: "400px" }}
             allowfullscreen="true"
             webkitallowfullscreen="true"
             mozallowfullscreen="true"
