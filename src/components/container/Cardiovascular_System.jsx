d6e35677d6900b80a8f886aea0dccaea7ac98018
Rajendra <rajendramohorana2001@gmail.com>
2024-05-14 17:49:58 +0530

    added

--- src/components/container/Cardiovascular_System.jsx
+++ src/components/container/Cardiovascular_System.jsx
@@ -4,7 +4,7 @@ const Cardio = () => {
     return (
         <div className='flex justify-center items-center h-screen'>
             <iframe
-                src="https://cardiovascular-system.vercel.app/"
+                src="https://cardio-eight.vercel.app/"
                 title="Heart"
                 style={{ width: "1000px", height: "700px" }}
                 allowfullscreen="true"
