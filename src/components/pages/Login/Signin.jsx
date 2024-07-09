1558b3cd1f264baf9aedec477ce69a93fd30d824
Rajendra <rajendramohorana2001@gmail.com>
2024-07-09 15:49:37 +0530

    added

--- src/components/pages/Login/Signin.jsx
+++ src/components/pages/Login/Signin.jsx
@@ -3,31 +3,29 @@ import { GoogleButton } from 'react-google-button';
 import { UserAuth } from '../../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
-const Signin = async () => {
-  // const { googleSignIn, user } = UserAuth();
-  // const navigate = useNavigate();
+const Signin = () => {
+  const { googleSignIn, user } = UserAuth();
+  const navigate = useNavigate();
 
-  // const handleGoogleSignIn = async () => {
-  //   try {
-  //     await googleSignIn();
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-  const credential = GoogleAuthProvider.credential(googleUser.getAuthResponse().id_token);
-  const result = await signInWithCredential(auth, credential);
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  // useEffect(() => {
-  //   if (user != null) {
-  //     navigate('/');
-  //   }
-  // }, [navigate, user]);
+  useEffect(() => {
+    if (user != null) {
+      navigate('/');
+    }
+  }, [navigate, user]);
 
   return (
     <div className='h-screen'>
       <h1 className='text-center text-3xl font-bold py-8'>Sign in</h1>
       <div className='max-w-[240px] m-auto py-4'>
-        {/* <GoogleButton onClick={handleGoogleSignIn} /> */}
+        <GoogleButton onClick={handleGoogleSignIn} />
       </div>
     </div>
   );
