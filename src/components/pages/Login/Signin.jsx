96f35270a3dd2fb83b0329cefbf1d43cdba02852
Rajendra <rajendramohorana2001@gmail.com>
2024-07-09 15:47:22 +0530

    added

--- src/components/pages/Login/Signin.jsx
+++ src/components/pages/Login/Signin.jsx
@@ -17,17 +17,17 @@ const Signin = async () => {
   const credential = GoogleAuthProvider.credential(googleUser.getAuthResponse().id_token);
   const result = await signInWithCredential(auth, credential);
 
-  useEffect(() => {
-    if (user != null) {
-      navigate('/');
-    }
-  }, [navigate, user]);
+  // useEffect(() => {
+  //   if (user != null) {
+  //     navigate('/');
+  //   }
+  // }, [navigate, user]);
 
   return (
     <div className='h-screen'>
       <h1 className='text-center text-3xl font-bold py-8'>Sign in</h1>
       <div className='max-w-[240px] m-auto py-4'>
-        <GoogleButton onClick={handleGoogleSignIn} />
+        {/* <GoogleButton onClick={handleGoogleSignIn} /> */}
       </div>
     </div>
   );
